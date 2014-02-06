48b4ce2e450ad430f48bc06a4f3c756179e5d047
Bryan Veloso <bryan@revyver.com>
2014-02-05 22:02:47 -0800

    Initial commit. Hello project!

---
+++ BroadcasterApplication.h
@@ -0,0 +1,15 @@
+//
+//  BroadcasterApplication.h
+//  Shiver Broadcaster
+//
+//  Created by Bryan Veloso on 2/5/14.
+//  Copyright (c) 2014 Bryan Veloso. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SBAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ BroadcasterApplication.m
@@ -0,0 +1,18 @@
+//
+//  BroadcasterApplication.m
+//  Shiver Broadcaster
+//
+//  Created by Bryan Veloso on 2/5/14.
+//  Copyright (c) 2014 Bryan Veloso. All rights reserved.
+//
+
+#import "BroadcasterApplication.h"
+
+@implementation SBAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
